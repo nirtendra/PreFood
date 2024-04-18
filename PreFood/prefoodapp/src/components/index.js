16be35ee39d89e0521c840361ea4c4ff767998f4
nirtendra <37367660+nirtendra@users.noreply.github.com>
2024-04-18 10:00:46 +1000

    Delete PreFood/prefoodapp directory

--- PreFood/prefoodapp/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { Header }  from "./Layouts/Header";
-export { Footer } from "./Layouts/Footer";
-
-export {ProductCard} from "./Elements/ProductCard"
-export {Rating} from "./Elements/Rating"
-export {ScrollToTop} from "./Other/ScrollToTop"
-
- 
--- PreFood/prefoodapp/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export {HomePage} from "./Home/HomePage";
-export { ProductsList } from "../pages/Products/ProductsList"
-export {ProductDetail} from "./ProductDetail"
